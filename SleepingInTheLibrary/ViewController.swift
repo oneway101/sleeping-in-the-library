185a37a5c3545ba2b600c1b6a9432ee4dca34fa0
oneway101 <oneway.first@gmail.com>
2017-01-07 14:33:14 -0500

    Escaping Parameters

--- SleepingInTheLibrary/ViewController.swift
+++ SleepingInTheLibrary/ViewController.swift
@@ -37,9 +37,32 @@ class ViewController: UIViewController {
             grabImageButton.alpha = 0.5
         }
     }
+    // MARK: Escaping Parameters
+    private func escapedParameters(_ parameters: [String:AnyObject]) -> String {
+        
+        if parameters.isEmpty {
+            return ""
+        } else {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                
+                // make sure that it is a string value
+                let stringValue = "\(value)"
+                
+                // escape it
+                let escapedValue = stringValue.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                
+                // append it
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+                
+            }
+            
+            return "?\(keyValuePairs.joined(separator: "&"))"
+        }
+    }
     
     // MARK: Make Network Request
-    
     private func getImageFromFlickr() {
         
     }
